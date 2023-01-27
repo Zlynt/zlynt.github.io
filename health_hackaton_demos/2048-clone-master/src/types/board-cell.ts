d21b930a25b4ca92121a66d101bb0dee1ccbdf61
Ivan Teixeira <thezlynt@gmail.com>
2023-01-27 16:24:19 +0000

    Hackaton

--- health_hackaton_demos/2048-clone-master/src/types/board-cell.ts
+++
@@ -1,8 +0,0 @@
-import { Coordinates } from './coordinates';
-
-export interface BoardCell {
-    value: number;
-    position: Coordinates | null;
-    lastMoveId: number;
-    canvasRef?: Path2D;
-}
--- health_hackaton_demos/2048-clone-master/src/types/position-2d.ts
+++
@@ -1,4 +0,0 @@
-export interface Position2D {
-    row: number;
-    col: number;
-}
